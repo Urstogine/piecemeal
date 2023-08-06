7340af48756efa4b6dc63b4a4e13bb702118a9c5
Urstogine <115228903+Urstogine@users.noreply.github.com>
2023-08-06 14:13:28 +0800

    Update coreUtil.java

--- src/main/java/com/urstogine/piecemeal/coremod/coreUtil.java
+++ src/main/java/com/urstogine/piecemeal/coremod/coreUtil.java
@@ -3,7 +3,5 @@ package com.urstogine.piecemeal.coremod;
 import net.minecraft.entity.EntityLivingBase;
 
 public class coreUtil {
-    public static float getHealth(EntityLivingBase living){
-        return 0.0f;
-    }
+    
 }
